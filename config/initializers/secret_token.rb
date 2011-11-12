e17a10aa082fde38114215d2100895803bd0534c
Eric Allen <ericpallen@gmail.com>
2011-11-12 09:26:41 -0800

    Rails 3.1.1 init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CucumberSeleniumDemo::Application.config.secret_token = '0d29d1f5c2b9bb361e499cd83b89c4275d71dbeafd4b83bbe230b499a2b927641e2a0f1f916c724aa571a6be0533cc17aba25658fa065b7683add6b5a00e80e2'
